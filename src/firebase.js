4f3d410453629866d02764ddfb8b8ceca19d29d6
KristinAlex1 <kristinalex2001@gmail.com>
2025-02-08 02:22:13 -0400

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDGs0GqBhugQ_GbXYMjK2ckCjwprq4aOz8",
+    authDomain: "netflix-clone-b312c.firebaseapp.com",
+    projectId: "netflix-clone-b312c",
+    storageBucket: "netflix-clone-b312c.firebasestorage.app",
+    messagingSenderId: "506410533260",
+    appId: "1:506410533260:web:28cb7158215c1a36cc56a9"
+  };

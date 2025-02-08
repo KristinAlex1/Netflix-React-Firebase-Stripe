5ad152e177694d8aec68fca868abb21a5ed118dc
KristinAlex1 <kristinalex2001@gmail.com>
2025-02-08 19:40:50 -0400

    Connected with TMDB API using axios

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,18 +1,39 @@
-import React, { useState } from 'react'
+import axios from '../src/axios';
+import React, { useEffect, useState } from 'react'
+import requests from '../src/Request';
 
 const Hero = () => {
+    const[movie,setMovie] = useState([]);
+
     const truncate = (string) => {
         return string?.length > 150 ? string.substr(0,150) + ' ...': string;
 
     }
 
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(requests.fetchNetflixOriginals);
+            setMovie(
+                request.data.results[
+                    Math.floor(Math.random() * (request.data.results.length - 1))
+                ]
+            );
+            return request;
+            
+        }
+        fetchData();
+
+    },[])
+
+    console.log(movie)
+
 
   return (
     <>
-        <header className='h-[50vh] 'style={{ 
+        <header className='h-[55vh] 'style={{ 
             backgroundSize: 'cover',
-            backgroundPosition: 'center center',
-            backgroundImage: `url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThCRGvp5vtgaHziVcGSg8kLaGT5F1qU0H8jA&s)` }}>
+            backgroundPosition: 'top center',
+            backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path})` }}>
                 <div className='flex flex-col w-[30vw] text-white pl-[8rem] pt-[15rem] mb-[3rem] '>
                     <h1 className='text-5xl font-bold'>Movie Name</h1>
                     <div className='flex mt-[1rem]'>
